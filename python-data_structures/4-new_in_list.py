5bd5ba057b8ea71061c064dcb07fb69406599410
holberton <cavidansgrv06@gmail.com>
2025-11-19 14:20:08 -0500

    task

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < len(my_list) and idx >= 0:
-        my_list[idx] = element
-        return my_list
-    return new_list
+        new_list[idx] = element
+        return new_list
+    return my_list
