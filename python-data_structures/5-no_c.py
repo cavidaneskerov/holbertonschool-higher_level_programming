81144b7001c790a5b5503574bcc1edf6fdc9827a
holberton <cavidansgrv06@gmail.com>
2025-11-19 14:33:28 -0500

    task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = list(my_string)
-    for i in my_strin:
-        if i == "c" or i == "C":
-            my_string.remove(i)
-    new_string = "".join(my_string)
-    return new_string
+    return "".join([ch for ch in my_string if ch not in ('c', 'C')])
