d1ee8fcd082ed8276744bb381500bbbceb42a70b
holberton <cavidansgrv06@gmail.com>
2025-11-19 14:31:43 -0500

    task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace("C", "")
-    my_string = my_string.replace("c", "")
-    return my_string
+    my_string = list(my_string)
+    for i in my_strin:
+        if i == "c" or i == "C":
+            my_string.remove(i)
+    new_string = "".join(my_string)
+    return new_string
