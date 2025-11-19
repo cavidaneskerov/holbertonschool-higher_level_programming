b72155985e3117d3536b4a4d687a00e452f682e2
holberton <cavidansgrv06@gmail.com>
2025-11-19 15:13:05 -0500

    task

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list = []:
+        return None
+    number = my_list[0]
+    for i in my_list:
+        if i > number:
+            number = i
+    return number
