1050ae31dc2ea67c1a725efea9d84f0d097cf750
holberton <cavidansgrv06@gmail.com>
2025-11-19 15:22:46 -0500

    task

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in my_list:
-        if i % 2 == 0:
-            my_list.append(True)
-        else:
-            my_list.append(False)
+    return [x % 2 == 0 for x in my_list]
