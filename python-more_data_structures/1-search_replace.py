1d9dd9a84ef32c047886be3e22e4cf93610ac3e1
holberton <cavidansgrv06@gmail.com>
2025-11-20 13:51:26 +0400

    task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in range (len(my_list)):
         if my_list[i] != search:
-            new_list[i] = my_list[i]
+            new_list.append(my_list[i])
         else:
             new_list.append(replace)
     return new_list
