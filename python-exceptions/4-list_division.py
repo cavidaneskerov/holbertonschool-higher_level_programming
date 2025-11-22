ebc0a291c7668abf9fd223c6ea52f4579e941717
holberton <cavidansgrv06@gmail.com>
2025-11-22 10:04:05 -0500

    task

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,8 +3,6 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
-                raise TypeError("wrong type")
             division = my_list_1[i] / my_list_2[i]
         except IndexError:
             print("out of range")
