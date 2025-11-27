e369cc2784853070528c5261790e797ee6f4b0ef
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-27 17:36:47 +0400

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -11,31 +11,38 @@ class Rectangle:
 
     @property
     def width(self):
+        """Retrieve width"""
         return self.__width
-    
+
     @width.setter
-    def width(self, value_1):
-        if not isinstance(value_1, int):
+    def width(self, value):
+        """Set width"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value_1 < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value_1
+        self.__width = value
+
     @property
     def height(self):
+        """Retrieve height"""
         return self.__height
-    
+
     @height.setter
-    def height(self, value_2):
-        if not isinstance(value_2, int):
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value_2 < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value_2
+        self.__height = value
 
     def area(self):
-      return self.width * self.height
+        """Returns rectangle area"""
+        return self.width * self.height
+
     def perimeter(self):
-      if self.width == 0 or self.height == 0:
-          return 0
-      else:
-          return (self.width + self.height) * 2
+        """Returns rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
