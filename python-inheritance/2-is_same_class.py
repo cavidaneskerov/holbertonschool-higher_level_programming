4ac486790156560fafc2cbf42d8d117a3557a596
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-28 23:15:38 +0400

    Update 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Inheritance with same classes"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
