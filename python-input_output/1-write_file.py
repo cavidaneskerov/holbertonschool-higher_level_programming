3b99247044874b74e8192855b59310ddd901df5f
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-29 21:39:51 +0400

    Create 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python classes for writing file"""
+
+
+def write_file(filename="", text=""):
+    """Function for reading"""
+    with open(filename, "w") as f:
+        f.write(text)
+        print(len(f.read()))
