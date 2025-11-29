ca2412e4b3bf2102e50ac80277598fabca9d4da1
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-29 20:13:08 +0400

    Create 10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Create square with the help of rectangle class """
+
+Rectangle = __import__("9-rectangle.py").Rectangle
+
+
+class Square(Rectangle):
+    """Class for creating Square"""
+    
+    def __init__(self, size):
+        """Initialize size with private atribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Implemented area method """
+        return self.__size ** 2
+   
