b06f7a554e1d5bd01650c22b7ed73c05f81d7dcc
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-29 22:18:05 +0400

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,10 @@ import sys
 from 5-save_to_json_file.py import save_to_json_file
 from 6-load_from_json_file.py import load_from_json_file
 
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
 items.extend(sys.argv[1:])
 save_to_json_file(items, add_item.json)
