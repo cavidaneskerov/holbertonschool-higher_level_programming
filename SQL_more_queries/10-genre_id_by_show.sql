c600c25b22c8bc142ada0fd2ba307195c2de7c32
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-30 16:49:46 +0400

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 --
-SElECT tv_shows.title, tv_show_genres.genre_id
-FROM tw_shows
-JOIN tw_show_genres ON tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
