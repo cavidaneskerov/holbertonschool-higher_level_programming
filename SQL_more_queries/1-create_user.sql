608510f0bcf72a12560eadc2de3f05be8dbb4193
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-30 16:00:25 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIViLEGES ON *.* TO 'user_0d_1'@'localhost';
