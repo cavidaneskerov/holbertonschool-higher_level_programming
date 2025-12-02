1a3aa15ddb02197a0995d6728938d00067d1a3f1
holberton <cavidansgrv06@gmail.com>
2025-12-02 14:15:47 +0400

    task

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Fetches a URL and displays the body (utf-8 decoded).
-Handles HTTP errors and prints the status code.
+Takes in a URL and an email, sends a POST request to the URL
+with the email as a parameter, and displays the body of the response.
 """
-import requests
+
 import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    url = sys.argv[2]
+    email = sys.argv[2]
 
     data = {"email": email}
-    r = requests.post(
-        url,
-        data=data,
-    )
-    print(r.text)
+    response = requests.post(url, data=data)
+
+    print(response.text)
