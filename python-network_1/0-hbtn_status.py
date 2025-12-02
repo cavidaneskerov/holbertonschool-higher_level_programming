5026b95459661a4c02c6e5fcc277ec552bc8c896
holberton <cavidansgrv06@gmail.com>
2025-12-02 11:56:12 +0400

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,4 +5,7 @@ url = "https://intranet.hbtn.io/status"
 with request.urlopen(url) as response:
     data = response.read()
 
-print(data)
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
