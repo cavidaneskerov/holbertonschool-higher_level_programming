f72650835bdbd0275b122dff09ffcc4d7ee00958
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:51:20 +0400

    task

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Fetches the X-Request-Id from a given URL
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers["X-Request-Id"])
