786d222744099c8426bc7d84ad1840594fd35b14
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:22:13 -0500

    task

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -17,27 +17,29 @@ def home():
     return "Welcome to the Flask API!"
 @app.route("/data")
 def data():
-    usernames = list(users.key())
+    usernames = list(users.keys())
     return jsonify(usernames)
 @app.route("/status")
 def status():
     return "OK"
 @app.route("/users/<username>")
+def userws():
     if username in username:
         return "User valid"
     else:
         return jsonify(user_error), 404
 @app.route("/add_user", methods = ["POST"])
-    def add_user():
-        new_user = request.get_json(silent=True)
-        username = new_user.get("username")
-        if new_user is None:
-            return jsonify(invalid_json), 400
-        if username == "":
-            return jsonify(user_req), 400
-        if username in usernames:
-            return jsonify(user_exist), 409
-        users[username] = new_user
+def add_user():
+    new_user = request.get_json(silent=True)
+    if new_user is None:
+        return jsonify(invalid_json), 400
+    username = new_user.get("username")
+    if not username:
+        return jsonify(user_req), 400
+    if username in users:
+        return jsonify(user_exist), 409
+    users[username] = new_user
+    return jsonify({"message": "User added", "user" : new_user}), 201
 
 if __name__ == "__main__":
     app.run()
