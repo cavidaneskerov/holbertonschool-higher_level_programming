1b9b55d3dbb5729cffbc7c084c0962e11358aae7
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:40:05 +0400

    task

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,5 +6,6 @@ Handles HTTP errors and prints the status code.
 import requests
 
 r = requests.get("https://intranet.hbtn.io/status")
+print("Body response:")
 print("-type:", type(r.text))
 print("-content:", r.text )
