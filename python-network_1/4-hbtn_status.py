f5db652e5da43aa3d34a8e2667d1c1d089d15f30
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:46:40 +0400

    task

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ import requests
 r = requests.get("https://intranet.hbtn.io/status")
 print("Body response:")
 print("\t- type:", type(r.text))
-print("\t- content:", r.text )
+print("\t- content:", r.text)
