ee5ed501a4c3cdbab5b6140a059b1edf2caf490e
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:15:25 +0400

    task

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Fetches and displays the X-Request-Id header from a given URL.
+"""
+from urllib import request, parse
+import sys
+
+if __name__ = "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    encoded = parse.urlencoe(email).encode("utf-8")
+    req = request.Request(url, data=encoded, methods="POST")
+
+with request.urlopen(req) as response:
+    print(response.read().decode("utf-8"))
