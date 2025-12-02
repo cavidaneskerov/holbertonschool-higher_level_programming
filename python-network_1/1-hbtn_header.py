c747da1cdefbfff3e6835e9c85bd24a976ae15c6
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:06:30 +0400

    task

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@ from urllib import request
 import sys
 
 url = sys.argv[1]
-with request.urlopen(url) as response
+with request.urlopen(url) as response:
     headers = response.info()
     header = headers.get("X-Request-Id")
     print(header)
