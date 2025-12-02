39f07da45c2e9e08eecf3fe115ab3525bbbcd9c5
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:38:35 +0400

    task

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Fetches a URL and displays the body (utf-8 decoded).
+Handles HTTP errors and prints the status code.
+"""
+import requests
+
+r = requests.get("https://intranet.hbtn.io/status")
+print("-type:", type(r))
+print("-content:", r.text )
