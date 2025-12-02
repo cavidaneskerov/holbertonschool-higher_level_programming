d1735f60394010746f5e6abfa64d313bd5d304b1
holberton <cavidansgrv06@gmail.com>
2025-12-02 13:14:53 -0500

    task

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Restful API and JWT tokens"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
