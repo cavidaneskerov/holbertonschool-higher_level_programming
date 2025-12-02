f4d7e3adc730eaa6e72fcb902d06faab0ad41d49
holberton <cavidansgrv06@gmail.com>
2025-12-02 14:04:52 +0400

    task

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -6,7 +6,7 @@ Handles HTTP errors and prints the status code.
 import requests
 import sys
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     url = sys.argv[2]
 
