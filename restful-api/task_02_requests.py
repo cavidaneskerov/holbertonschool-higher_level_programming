34fc7c91891f64efd1c3d700151350ab02f09d32
holberton <cavidansgrv06@gmail.com>
2025-12-02 16:55:23 +0400

    task

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""dfjsdfdshfjsdhfj skjsd """
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+def fetch_and_print_posts():
+    """Fetch and print posts"""
+    r = requests.get(url)
+    print(f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        data = r.json()
+        for post in data:
+            print(post.get("title"))
+
+def fetch_and_save_posts():
+    """Fetch and save posts"""
+    r = requests.get(url)
+    if r.status_code == 200:
+        print("Status Code:" r.status_code)
+        data = r.json()
+        list = []
+        for post in data:
+            list.append({
+                "id": post.get("id"),
+                "title": post.get("title"),
+                "body": post.get("body")
+            })
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerow(list)
