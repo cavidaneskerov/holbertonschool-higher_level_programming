0cabe3d116a11ee10d1664ee992c1ff3de83a2e4
holberton <cavidansgrv06@gmail.com>
2025-12-02 12:31:05 +0400

    task

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a given URL with an email parameter.
+"""
+import urllib
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
