f26b50d599484fd8eea88e0fe578d39ff1d8c507
holberton <cavidansgrv06@gmail.com>
2025-12-03 11:00:39 -0500

    task

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Template with Jinja"""
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def item():
+    with open("items.json", "r") as file:
+        json_file = json.load(file)
+        items = json_file["items"]
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
